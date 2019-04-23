2861dcef1f3d05db089d21f18745249bb4c615ef
Romain Lods <rlods@deezer.com>
2019-04-23 23:05:55 +0200

    Fix menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 (function() {
-  const infos = {};
+  const blk_infos = {};
+  const nav_infos = {};
 
   window.addEventListener("load", () => {
-    infos["Info1"] = document.getElementById("Info1");
-    infos["Info2"] = document.getElementById("Info2");
-    infos["Info3"] = document.getElementById("Info3");
+    blk_infos["Info1"] = document.getElementById("Info1");
+    blk_infos["Info2"] = document.getElementById("Info2");
+    blk_infos["Info3"] = document.getElementById("Info3");
+    nav_infos["Info1"] = document.getElementById("NavItem1");
+    nav_infos["Info2"] = document.getElementById("NavItem2");
+    nav_infos["Info3"] = document.getElementById("NavItem3");
   });
 
-  window.toggleInfo = id => {
-    Object.values(infos).forEach(info => {
-      if (info.id !== id) info.classList.remove("visible");
+  window.toggleInfo = infoId => {
+    Object.values(blk_infos).forEach(blk_info => {
+      blk_info.classList.remove("visible");
     });
-    infos[id].classList.toggle("visible");
+    Object.values(nav_infos).forEach(nav_info => {
+      nav_info.classList.add("disabled");
+    });
+    blk_infos[infoId].classList.add("visible");
+    nav_infos[infoId].classList.remove("disabled");
   };
 })();
